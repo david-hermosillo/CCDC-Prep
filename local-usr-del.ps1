a3399bc113d82c760ca57ced5d8bcacadff53cd2
david-hermosillo <75981800+david-hermosillo@users.noreply.github.com>
2025-09-21 11:35:08 -0700

    Add files via upload

---
+++ local-usr-del.ps1
@@ -0,0 +1,12 @@
+﻿$userfile = "C:\temp\users.txt"
+
+# Read each line from file
+Get-Content $userfile | ForEach-Object {
+    $user = $_.Trim()
+    if (Get-LocalUser -Name $user -ErrorAction SilentlyContinue) {
+        Write-Host "Deleting user: $user"
+        Remove-LocalUser -Name $user
+    } else {
+        Write-Host "User $user does not exist."
+    }
+}
